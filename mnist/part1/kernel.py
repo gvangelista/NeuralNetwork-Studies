58e943f4c8ac8075f8e383b1e5acc8faff843b72
Gabriel Evangelista <gabriel.evanbs@gmail.com>
2021-04-25 20:57:37 -0400

    Começo dos testes no git

---
+++ mnist/part1/kernel.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+### Functions for you to fill in ###
+
+
+
+def polynomial_kernel(X, Y, c, p):
+    """
+        Compute the polynomial kernel between two matrices X and Y::
+            K(x, y) = (<x, y> + c)^p
+        for each pair of rows x in X and y in Y.
+
+        Args:
+            X - (n, d) NumPy array (n datapoints each with d features)
+            Y - (m, d) NumPy array (m datapoints each with d features)
+            c - a coefficient to trade off high-order and low-order terms (scalar)
+            p - the degree of the polynomial kernel
+
+        Returns:
+            kernel_matrix - (n, m) Numpy array containing the kernel matrix
+    """
+    # YOUR CODE HERE
+    K = X @ Y.transpose()
+    K += c
+    K **= p
+    return K
+    raise NotImplementedError
+
+
+
+def rbf_kernel(X, Y, gamma):
+    """
+        Compute the Gaussian RBF kernel between two matrices X and Y::
+            K(x, y) = exp(-gamma ||x-y||^2)
+        for each pair of rows x in X and y in Y.
+
+        Args:
+            X - (n, d) NumPy array (n datapoints each with d features)
+            Y - (m, d) NumPy array (m datapoints each with d features)
+            gamma - the gamma parameter of gaussian function (scalar)
+
+        Returns:
+            kernel_matrix - (n, m) Numpy array containing the kernel matrix
+    """
+    # YOUR CODE HERE
+    n = X.shape[0]
+    m = Y.shape[0]
+    K = np.exp(-gamma * (np.matmul(np.sum(X * X, axis=1).reshape(-1, 1), np.ones((1, m))) + np.matmul(np.ones((n, 1)),np.sum(Y * Y,axis=1).reshape((1,m))) - 2 * np.matmul( X, np.transpose(Y))))
+    return K
+    raise NotImplementedError
